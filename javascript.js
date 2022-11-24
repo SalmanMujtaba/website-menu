b77889c6e2f6c283fa42ba4b9d5f583013fa47c1
Salman Mujtaba <salmanmujtaba@macbook.local>
2022-11-24 00:01:57 -0500

    Final update with css properties

--- javascript.js
+++ javascript.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const menuItems = document.getElementsByClassName("menu__item");
   menuItems && Array.from(menuItems).forEach((element, index) => {
     element.onmouseover = () => {
-      menu.dataset.activeIndex = index;
+      menu.style.setProperty("--active-index", index);
     };
   });
 });
